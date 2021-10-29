85be0e958bbe0005ec6dbe4d7d56d25699c55e4c
shalabhraj1990 <shalabhrajetc@gmail.com>
2021-10-29 10:28:47 +0530

    scripts initail commmit

---
+++ index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+//const port = process.env.PORT || 8080;
+const port = process.env.npm_package_config_port || 8080 // using package.json config
+let runningMessage = `server is running on port ${port}`;
+
+app.get('/',(req,res) => {
+    console.log('API was successfully requested');
+    res.send(runningMessage);
+});
+
+const server = app.listen(port,() => {
+    console.log(runningMessage);
+});
+
+module.exports = server;
